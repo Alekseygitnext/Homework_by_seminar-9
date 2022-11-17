bfea91f0ab56402634b32cf65b88807fccb45722
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-11-17 21:54:49 +1000

    задача 1 к семинару № 9

---
+++ task1_by_seminar 9/Program.cs
@@ -0,0 +1,13 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+
+void PrintNumber(int M, int N)
+{
+    if (N < M) return;
+    PrintNumber(M, N - 1);
+    Console.Write(N + " ");
+}
+Console.Write("Введите число M : ");
+int M = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число N : ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+PrintNumber(M, N);
