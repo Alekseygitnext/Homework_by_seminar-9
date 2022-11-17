d6a4d63fca692e3fd17ebcdaaab63f7b7bda403b
Aleksey Gvozdev <gvozd0227yandex.ru>
2022-11-17 22:11:32 +1000

    задача 2 к семинару № 9

---
+++ task2_by_seminar 9/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму элементов в промежутке от M до N, N и M заданы
+
+int Sum(int M, int N)
+{
+    if (N == M) return N;
+    return M + Sum(M + 1, N);
+}
+Console.Write("Введите число M : ");
+int M = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите число N : ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine($"Сумма элементов: {Sum(M, N)}");
